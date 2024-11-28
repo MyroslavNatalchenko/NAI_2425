3f714b2ab71a72250a9209a776a87657db5a215b
Myroslav Natalchenko <99477515+MyroslavNatalchenko@users.noreply.github.com>
2024-11-28 14:45:29 +0100

    Create detect.py

---
+++ PyTorch_9/detect.py
@@ -0,0 +1,36 @@
+import sys
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import cv2
+
+from main import test_data, device, NeuralNetwork
+
+model = NeuralNetwork().to(device)
+model.load_state_dict(torch.load("model.pth", weights_only=True))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+image_path = sys.argv[1]
+image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+image = cv2.resize(image, (28, 28))
+tensor_img = torch.tensor(image, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+
+model.eval()
+with torch.no_grad():
+    pred = model(tensor_img)
+    predicted = classes[pred[0].argmax(0)]
+    print(f'Predicted: "{predicted}"')
